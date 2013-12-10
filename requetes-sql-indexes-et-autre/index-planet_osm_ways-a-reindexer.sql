8c4186943ca67fea51d628253afbf6bd26badcb3
sletuffe <git@letuffe.org>
2013-12-10 15:27:06 +0100

    req SQL pour ré-indexer l'index where pending de planet_osm_ways

---
+++ requetes-sql-indexes-et-autre/index-planet_osm_ways-a-reindexer.sql
@@ -0,0 +1,2 @@
+-- cet index foire après chaque importation intiale, je pige pas pourquoi... mais le réindexer permet de gérer de where pending sans seqential scan
+reindex index planet_osm_ways_idx;
