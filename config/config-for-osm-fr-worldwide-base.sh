242815fff01903c8c401af89eafdabbb260d39e7
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2019-11-02 17:14:54 +0100

    Config osmfr monde mise à jour pour osm165

--- config/config-for-osm-fr-worldwide-base.sh
+++ config/config-for-osm-fr-worldwide-base.sh
@@ -2,14 +2,14 @@
 project_dir=$(dirname $0)
 
 #If those are in your path just set :
-#osm2pgsql=osm2pgsql
-#osmosis=osmosis
+osm2pgsql=osm2pgsql
+osmosis=osmosis
 #if you want relative path use $project_dir instead like $project_dir/../path-to-binary or $project_dir/path-to-binary
 #$project_dir beeing the directory this config.sh file is
 
 #binary paths
-osm2pgsql=$project_dir/../osm2pgsql/osm2pgsql
-osmosis=$project_dir/../osmosis-0.43.1/bin/osmosis
+#osm2pgsql=$project_dir/../osm2pgsql/osm2pgsql
+#osmosis=$project_dir/../osmosis-0.43.1/bin/osmosis
 
 #database name to choose
 base_osm=osm
@@ -30,9 +30,9 @@ with_timeings=1
 
 #For osm2pgsql options (database name is not hardcoded here because some scripts needs it as a variable, so we just make use of it here)
 #--tag-transform-script $project_dir/script.lua (pensez à faire des chemin absolu ou utilisez $project_dir sinon, ça foire quand c'est pas lancé du dossier en cours)
-common_osm2pgsql_options=" -k -m -G -s -S $project_dir/osm2pgsql-choosen.style -d $base_osm --keep-coastlines --flat-nodes /ssd/osm2pgsql/flat-nodes.raw --tag-transform-script $project_dir/config/activate-relation-type-waterway.lua"
-diff_osm2pgsql_options="--number-processes=4 -a -C 64 $common_osm2pgsql_options"
-import_osm2pgsql_options="--create -C 18000 --number-processes=8 $common_osm2pgsql_options --tablespace-main-data ssd --tablespace-main-index ssd --tablespace-slim-data ssd --tablespace-slim-index ssd "
+common_osm2pgsql_options=" -k -m -G -s -S $project_dir/osm2pgsql-choosen.style -d $base_osm --keep-coastlines --flat-nodes /ssd-nvme/osm2pgsql/flat-nodes.raw"
+diff_osm2pgsql_options="--number-processes=12 -a -C 1024 $common_osm2pgsql_options"
+import_osm2pgsql_options="--create -C 18000 --number-processes=12 $common_osm2pgsql_options "
 
 #post import sql scripts in requetes-sql-indexes-et-autre to run, separated by spaces
 operations_post_import="index-planet_osm_ways-a-reindexer.sql indexes-admin_level.sql indexes-ref.sql index-ref-sandre.sql"
@@ -46,7 +46,7 @@ osm2pgsql_expire_tile_list=$work_dir/expire.list
 #List of rendering style to run thru the render_expired commands
 #Be sure that this script as the filesystem rights to access tiles 
 #if empty, no expiration will occure, you'll have to do it with the expiry tile files in an other way
-rendering_styles_tiles_to_expire=$(grep "^\[" /etc/renderd.conf | egrep -v "(renderd|mapnik)" | cut -d"[" -f2 | cut -d"]" -f1)
+rendering_styles_tiles_to_expire=""
 render_expired_options="--min-zoom=12 --touch-from=12 --max-zoom=20"
 
 #You can use this to execute the render_expired with another user like "sudo -u www-data"
