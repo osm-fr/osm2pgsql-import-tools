2458ccde29985a05eba2211b8c8cbb76d9549368
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2019-11-02 17:14:48 +0100

    Simplification de import.sh pour optimiser l'import de .pbf

--- import.sh
+++ import.sh
@@ -3,36 +3,16 @@
 . $(dirname $0)/config.sh
 
 if [ "a$1" == "a" ]; then
-echo "usage : ./import.sh <.bz2 or .pbf file to import (can be an http/https/ftp url or local file) >"
+echo "usage : ./import.sh <.pbf file to import>"
 echo "ex : ./import.sh /here/is/my/file.pbf"
-echo "ex : ./import.sh http://site/file.pbf"
 exit
 fi
 
-filename=$(basename "$1")
-extension="${filename##*.}"
+parsing_mode="pbf"
 
-if [ $extension == "pbf" ] ; then
-  parsing_mode="pbf"
-  #FIXME I can't find a way to pass the | in the variable
-  external_bunzip2="cat"
-elif [ $extension == "bz2" ] ; then
-  external_bunzip2="bunzip2 -c"
-  parsing_mode="libxml2"
-else
-  external_bunzip2="cat" 
-  parsing_mode="libxml2"
-fi
+echo $osm2pgsql $import_osm2pgsql_options -r $parsing_mode $1
 
-if [[ $1 =~ "http://" ]] || [[ $1 =~ "ftp://" ]] || [[ $1 =~ "https://" ]] ; then
-  data_pipe="wget -q -O - $1 "
-else
-  data_pipe="cat $1 "
-fi
-
-echo $data_pipe $osm2pgsql $import_osm2pgsql_options -r $parsing_mode /dev/stdin
-
-$data_pipe | $external_bunzip2 | $osm2pgsql $import_osm2pgsql_options -r $parsing_mode /dev/stdin
+$osm2pgsql $import_osm2pgsql_options -r $parsing_mode $1
 
 # running post import sql scripts (will do nothing if no sql scripts configured in config.sh)
 $project_dir/apply-post-import-sql.sh
