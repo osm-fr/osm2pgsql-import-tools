434b9b286c7dc7dac86a4ad8d2c0991fd2b1713e
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2019-12-19 16:19:57 +0100

    Ajout flock sur render_list.sh
    Fix osm-fr/infrastructure#125

--- render_list.sh
+++ render_list.sh
@@ -1,7 +1,12 @@
 #! /bin/sh
 
+(
+flock -n -x 2004
+
 for i in $(grep "^\[" /etc/renderd.conf | egrep -v "(renderd|mapnik)" | cut -d"[" -f2 | cut -d"]" -f1); do
   echo $(date "+%Y-%m-%d %H:%M:%S") $i
   sudo -u www-data ../mod_tile/render_list -m $i -a -f -z 1 -Z 11 -n 8 -l 12 -s /var/run/renderd/renderd.sock
   echo $(date "+%Y-%m-%d %H:%M:%S") done $i
 done
+
+) 2004>/var/lock/osm2pgsql-render_list
